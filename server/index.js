7788bd5d0822267232ccf747de68653e5a4ff0e1
HamzahGX <141028491+HamzahGX@users.noreply.github.com>
2023-10-03 20:47:36 +0100

    updated version of chat app

--- server/index.js
+++ server/index.js
@@ -1,49 +1,65 @@
-import express from 'express'
-import { Server } from "socket.io"
-import path from 'path'
-import { fileURLToPath } from 'url'
+import express from 'express';
+import { Server } from 'socket.io';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+// Define __filename and __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 3500
+const PORT = process.env.PORT || 3500;
 
-const app = express()
+const app = express();
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, 'public')));
 
 const expressServer = app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-})
+    console.log(`listening on port ${PORT}`);
+});
 
 const io = new Server(expressServer, {
     cors: {
-        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
-    }
-})
+        origin: process.env.NODE_ENV === 'production' ? false : ['http://localhost:5500', 'http://127.0.0.1:5500'],
+    },
+});
 
-io.on('connection', socket => {
-    console.log(`User ${socket.id} connected`)
+// Create an array to store authenticated usernames
+const authenticatedUsers = [];
 
-    // Upon connection - only to user 
-    socket.emit('message', "Welcome to Chat App!")
+io.on('connection', (socket) => {
+    console.log(`User ${socket.id} connected`);
 
-    // Upon connection - to all others 
-    socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)}} connected`)
+    // Handle username authentication
+    socket.on('authenticate', (username) => {
+        if (authenticatedUsers.includes(username)) {
+            // Username is already taken
+            socket.emit('authenticationError', 'Username is already taken. Please choose another.');
+        } else {
+            // Store the authenticated username and inform the client
+            authenticatedUsers.push(username);
+            socket.emit('authenticationSuccess', `Welcome, ${username}!`);
+        }
+    });
 
-    // Listening for a message event 
-    socket.on('message', data => {
-        console.log(data)
-        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
-    })
+    // Upon connection - only to user
+    socket.emit('username', 'Hello and welcome to Chatterbox!');
 
-    // When user disconnects - to all others 
+    // Upon connection - to all others
+    socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} connected`);
+
+    // Listening for a message event
+    socket.on('message', (data) => {
+        console.log(data);
+        io.emit('message', { username: data.username, message: data.message }); // Include username
+    });
+
+    // When a user disconnects - to all others
     socket.on('disconnect', () => {
-        socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)}} disconnected`)
-    })
+        socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} disconnected`);
+    });
 
-    // Listen for activity 
+    // Listen for activity
     socket.on('activity', (name) => {
-        socket.broadcast.emit('activity', name)
-    })
-})
+        socket.broadcast.emit('activity', name);
+    });
+});
--- server/public/app.js
+++ server/public/app.js
@@ -1,39 +1,62 @@
-const socket = io('ws://localhost:3500')
+const socket = io('ws://localhost:3500');
 
-const activity = document.querySelector('.activity')
-const msgInput = document.querySelector('input')
+const activity = document.querySelector('.activity');
+const msgInput = document.querySelector('#message-input');
+const sendButton = document.querySelector('#send-button');
+const usernameInput = document.getElementById('username');
+
+let username = ''; // Store the authenticated username
 
 function sendMessage(e) {
-    e.preventDefault()
-    if (msgInput.value) {
-        socket.emit('message', msgInput.value)
-        msgInput.value = ""
+    e.preventDefault();
+    if (msgInput.value && username) {
+        socket.emit('message', { username, message: msgInput.value });
+        msgInput.value = '';
     }
-    msgInput.focus()
+    msgInput.focus();
 }
 
-document.querySelector('form')
-    .addEventListener('submit', sendMessage)
+sendButton.addEventListener('click', sendMessage);
 
-// Listen for messages 
-socket.on("message", (data) => {
-    activity.textContent = ""
-    const li = document.createElement('li')
-    li.textContent = data
-    document.querySelector('ul').appendChild(li)
-})
+// Handle username authentication
+const authForm = document.getElementById('auth-form');
+
+authForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    username = usernameInput.value;
+
+    // Send the username to the server for authentication
+    socket.emit('authenticate', username);
+});
+
+// Listen for authentication success and error only once
+socket.once('authenticationSuccess', (message) => {
+    alert(message);
+});
+
+socket.once('authenticationError', (message) => {
+    alert(message);
+});
+
+// Listen for messages
+socket.on('message', (data) => {
+    activity.textContent = '';
+    const li = document.createElement('li');
+    li.textContent = `${data.username}: ${data.message}`;
+    document.querySelector('ul').appendChild(li);
+});
 
 msgInput.addEventListener('keypress', () => {
-    socket.emit('activity', socket.id.substring(0, 5))
-})
+    socket.emit('activity', socket.id.substring(0, 5));
+});
 
-let activityTimer
-socket.on("activity", (name) => {
-    activity.textContent = `${name} is typing...`
+let activityTimer;
+socket.on('activity', (username) => {
+    activity.textContent = `${username} is typing...`;
 
     // Clear after 3 seconds 
-    clearTimeout(activityTimer)
+    clearTimeout(activityTimer);
     activityTimer = setTimeout(() => {
-        activity.textContent = ""
-    }, 3000)
-})
+        activity.textContent = '';
+    }, 3000);
+});
